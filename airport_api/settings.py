5c145846eeab575b58d2a87640023471756c6a1d
Andrii Homeniuk <andreygomenuk@gmail.com>
2026-07-23 15:35:50 +0300

    feat(user): configure custom User model
    - Add 'user' app to INSTALLED_APPS
    - Implement User model extending AbstractUser
    - Set AUTH_USER_MODEL = "user.User" in settings
    - Prepare project for custom authentication and user management

--- airport_api/settings.py
+++ airport_api/settings.py
@@ -43,7 +43,10 @@ INSTALLED_APPS = [
     "rest_framework",
     "debug_toolbar",
     "airport",
+    "user",
 ]
+
+AUTH_USER_MODEL = "user.User"
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
