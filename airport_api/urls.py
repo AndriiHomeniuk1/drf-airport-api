40fff4467a8467efb2c091ec93b1c441477fbf02
Andrii Homeniuk <andreygomenuk@gmail.com>
2026-08-09 13:07:23 +0300

    chore(urls): conditionally include debug toolbar
    - Move debug_toolbar route registration under settings.DEBUG check
    - Ensure "__debug__/" URL is only available in development mode
    - Prevent exposing debug toolbar in production environment

--- airport_api/urls.py
+++ airport_api/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import (
@@ -28,7 +28,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/airport/", include("airport.urls", namespace="airport")),
     path("api/user/", include("user.urls", namespace="user")),
-    path("__debug__/", include("debug_toolbar.urls")),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
         "api/doc/swagger/",
@@ -41,3 +40,8 @@ urlpatterns = [
         name="redoc"
     ),
 ]
+
+if settings.DEBUG:
+    urlpatterns += [
+        path("__debug__/", include("debug_toolbar.urls")),
+    ]
